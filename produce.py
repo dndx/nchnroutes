c45a1b2e285f5383f6abe5bd17daa8c5e6e190aa
jellybean13 <jellybean13@lan>
2022-08-02 12:47:14 +0800

    Re-organize a list containing reserved IPv4 addresses

--- produce.py
+++ produce.py
@@ -43,22 +43,20 @@ def dump_bird(lst, f):
 RESERVED = [
     IPv4Network('0.0.0.0/8'),
     IPv4Network('10.0.0.0/8'),
+    IPv4Network('100.64.0.0/10'),
     IPv4Network('127.0.0.0/8'),
     IPv4Network('169.254.0.0/16'),
     IPv4Network('172.16.0.0/12'),
-    IPv4Network('192.0.0.0/29'),
-    IPv4Network('192.0.0.170/31'),
+    IPv4Network('192.0.0.0/24'),
     IPv4Network('192.0.2.0/24'),
     IPv4Network('192.168.0.0/16'),
     IPv4Network('198.18.0.0/15'),
     IPv4Network('198.51.100.0/24'),
     IPv4Network('203.0.113.0/24'),
+    IPv4Network('224.0.0.0/4'),
+    IPv4Network('233.252.0.0/24'),
     IPv4Network('240.0.0.0/4'),
     IPv4Network('255.255.255.255/32'),
-    IPv4Network('169.254.0.0/16'),
-    IPv4Network('127.0.0.0/8'),
-    IPv4Network('224.0.0.0/4'),
-    IPv4Network('100.64.0.0/10'),
 ]
 RESERVED_V6 = []
 if args.exclude:
